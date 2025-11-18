09d973b96cfe0d99df35c518d673dbe5680fc8f9
S-Suwachai <Suwachai.salatip@gmail.com>
2025-11-19 00:21:41 +0700

    add MBRF and AOD in combine line

--- local-backend/server.js
+++ local-backend/server.js
@@ -15,14 +15,18 @@ app.use("/nat/tn/tn-daily", require("./api_nat/tn_tn_daily"));
 app.use("/nat/tn/tn-summary", require("./api_nat/tn_tn_summary"));
 
 app.use("/nat/gd/2ndinbore-realtime", require("./api_nat/gd_2ndInBore_realtime").router);
+app.use("/nat/gd/2ndinbore-daily", require("./api_nat/gd_2gdInBore_daily"));
 app.use("/nat/gd/2ndinrace-realtime", require("./api_nat/gd_2ndInRace_realtime").router);
+app.use("/nat/gd/2ndinrace-daily", require("./api_nat/gd_2gdInRace_daily"));
 app.use("/nat/gd/2ndinsuper-realtime", require("./api_nat/gd_2ndInSuper_realtime").router);
+app.use("/nat/gd/2ndinsuper-daily", require("./api_nat/gd_2gdInSuper_daily"));
 app.use("/nat/gd/2ndoutsuper-realtime", require("./api_nat/gd_2ndOutSuper_realtime").router);
 app.use("/nat/gd/2ndoutrace-realtime", require("./api_nat/gd_2ndOutRace_realtime").router);
 app.use("/nat/gd/1gd-sum-alarm", require("./api_nat/gd_1gd_sum_alarm"));
 app.use("/nat/gd/2gd-sum-alarm", require("./api_nat/gd_2gd_sum_alarm"));
 
 app.use("/nat/assy/mbr-realtime", require("./api_nat/assy_mbr_realtime").router);
+app.use("/nat/assy/mbrf-realtime", require("./api_nat/assy_mbrf_realtime").router);
 app.use("/nat/assy/mbr-sum-alarm", require("./api_nat/assy_mbr_sum_alarm"));
 app.use("/nat/assy/mbr-daily", require("./api_nat/assy_mbr_daily"));
 app.use("/nat/assy/arp-realtime", require("./api_nat/assy_arp_realtime").router);
@@ -38,8 +42,10 @@ app.use("/nat/assy/ant-realtime", require("./api_nat/assy_ant_realtime").router)
 app.use("/nat/assy/ant-sum-alarm", require("./api_nat/assy_ant_sum_alarm"));
 app.use("/nat/assy/aod-realtime", require("./api_nat/assy_aod_realtime").router);
 app.use("/nat/assy/avs-realtime", require("./api_nat/assy_avs_realtime").router);
+app.use("/nat/assy/avs-daily", require("./api_nat/assy_avs_daily"));
 app.use("/nat/assy/avs-sum-alarm", require("./api_nat/assy_avs_sum_alarm"));
 app.use("/nat/assy/alu-realtime", require("./api_nat/assy_alu_realtime").router);
+app.use("/nat/assy/alu-daily", require("./api_nat/assy_alu_daily"));
 app.use("/nat/assy/alu-sum-alarm", require("./api_nat/assy_alu_sum_alarm"));
 
 app.use("/nat/assy/combine-realtime", require("./api_nat/assy_combine_realtime"));
