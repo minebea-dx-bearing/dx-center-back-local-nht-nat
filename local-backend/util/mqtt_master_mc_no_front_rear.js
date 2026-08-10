780bdc461e03a655b786fe1b7fbda22e2f497861
WaraleeJ <jellywaralee@gmail.com>
2026-08-10 13:41:58 +0700

    alu, avs using status

--- local-backend/util/mqtt_master_mc_no_front_rear.js
+++ local-backend/util/mqtt_master_mc_no_front_rear.js
@@ -69,6 +69,58 @@ const master_mc_no = async (dbms, DATABASE_PROD, DATABASE_ALARM, DATABASE_MASTER
       `
     );
 
+    // `
+    //     WITH LatestProduction AS (
+    //         SELECT
+    //             *
+    //             ,ROW_NUMBER() OVER (PARTITION BY [mc_no] ORDER BY [registered] DESC) AS rn
+    //         FROM ${DATABASE_PROD}
+    //         WHERE [registered] >= DATEADD(day, -3, GETDATE())
+    //     ),
+    //     Lateststatus AS (
+    //         SELECT
+    //             [mc_no],
+    //             [mc_status],
+    //             [occurred],
+    //             CASE
+    //                 WHEN UPPER([mc_status]) LIKE '%REAR%' THEN 'REAR'
+    //                 WHEN UPPER([mc_status]) LIKE '%FRONT%' THEN 'FRONT'
+    //             END AS status_type,
+    //             ROW_NUMBER() OVER ( PARTITION BY [mc_no],
+    //                 CASE
+    //                     WHEN UPPER([mc_status]) LIKE '%REAR%' THEN 'REAR'
+    //                     WHEN UPPER([mc_status]) LIKE '%FRONT%' THEN 'FRONT'
+    //                 END
+	// 				ORDER BY [occurred] DESC
+    //             ) AS rn
+    //         FROM ${DATABASE_STATUS}
+    //         WHERE
+    //             UPPER([mc_status]) LIKE '%RUN%'
+    //             AND [occurred] >= DATEADD(day, -3, GETDATE())
+    //     ),
+    //     Pivotedstatuss AS (
+    //         SELECT
+    //             [mc_no],
+    //             MAX(CASE WHEN status_type = 'FRONT' THEN [mc_status] END) AS status_front,
+    //             MAX(CASE WHEN status_type = 'FRONT' THEN [occurred] END) AS occurred_front,
+    //             MAX(CASE WHEN status_type = 'REAR' THEN [mc_status] END) AS status_rear,
+    //             MAX(CASE WHEN status_type = 'REAR' THEN [occurred] END) AS occurred_rear
+    //         FROM Lateststatus
+    //         WHERE rn = 1  -- เอาเฉพาะ status ล่าสุดของแต่ละประเภท
+    //         GROUP BY [mc_no] -- รวมข้อมูลให้เหลือ mc_no ละ 1 แถว
+    //     )
+    //     SELECT 
+    //         p.*, -- เลือกทุกคอลัมน์จาก Production
+    //         ISNULL(a.status_front, 'no data run') AS status_front,
+    //         a.occurred_front,
+    //         ISNULL(a.status_rear, 'no data run') AS status_rear,
+    //         a.occurred_rear
+    //     FROM LatestProduction p
+    //     LEFT JOIN Pivotedstatuss a ON p.[mc_no] = a.[mc_no] COLLATE Thai_CI_AS
+    //     WHERE p.rn = 1
+    //     ORDER BY p.[mc_no];
+    //   `
+
     // dbms.query จะคืนค่าเป็น [results, metadata]
     return result[0];
   } catch (error) {
