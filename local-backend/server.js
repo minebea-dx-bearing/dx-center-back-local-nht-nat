910fbcb66afc3b23d1f7ba1f0e1fd498fbc2aea8
peter <phatsawut.phuket1@gmail.com>
2026-08-26 09:55:13 +0700

    fix(nht): enhance prepareRealtimeData function and restore AOD route in server

--- local-backend/server.js
+++ local-backend/server.js
@@ -81,7 +81,7 @@ app.use("/nht/assy/mbrf-realtime", require("./api_nht/assy_mbrf_realtime").route
 app.use("/nht/assy/fim-realtime", require("./api_nht/assy_fim_realtime").router);
 app.use("/nht/assy/gssm-realtime", require("./api_nht/assy_gssm_realtime").router);
 app.use("/nht/assy/ant-realtime", require("./api_nht/assy_ant_realtime").router);
-//app.use("/nht/assy/aod-realtime", require("./api_nht/assy_aod_realtime").router);
+app.use("/nht/assy/aod-realtime", require("./api_nht/assy_aod_realtime").router);
 app.use("/nht/assy/avs-realtime", require("./api_nht/assy_avs_realtime").router);
 app.use("/nht/assy/alu-realtime", require("./api_nht/assy_alu_realtime").router);
 app.use("/nht/assy/mbr-analysis-by-mc", require("./api_nht/assy_mbr_analysis_by_mc"));
